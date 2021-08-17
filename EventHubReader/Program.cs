0ebb038c58fe96876f5051244b69ac6476ab9237
Alexander Seminjakiw <alexander.seminjakiw@evopro-ag.de>
2021-08-17 13:58:40 +0200

    Add solution

---
+++ EventHubReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventHubReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
