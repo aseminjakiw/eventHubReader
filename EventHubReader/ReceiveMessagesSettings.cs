f12b705d0098286ca7787ff07373c5024397ec9b
Alexander Seminjakiw <alexander.seminjakiw@evopro-ag.de>
2021-08-17 17:12:47 +0200

    Update help description

--- EventHubReader/ReceiveMessagesSettings.cs
+++ EventHubReader/ReceiveMessagesSettings.cs
@@ -8,20 +8,20 @@ namespace EventHubReader
 {
     public class ReceiveMessagesSettings : CommandSettings
     {
-        [Description("Azure Event Hub connection string as presented in the Azure portal. Must contain Event Hub name.")]
-        [CommandArgument(0, "[consumerGroup]")]
+        [Description("Azure Event Hub connection string as presented in the Azure portal")]
+        [CommandArgument(0, "[connectionString]")]
         public string? ConnectionString { get; set; }
 
-        [Description("Consumer group of Event hub to read from.")]
+        [Description("Consumer group of Event Hub to read from. Default value '" + EventHubConsumerClient.DefaultConsumerGroupName + "'")]
         [CommandOption("-g|--consumer-group")]
         [DefaultValue(typeof(string), EventHubConsumerClient.DefaultConsumerGroupName)]
         public string ConsumerGroup { get; set; } = null!;
 
-        [Description("Only messages which contain all strings will be printed.")]
+        [Description("Only messages which contain this string will be printed. Can be added multiple times")]
         [CommandOption("-c|--contains")]
         public string[] Contains { get; set; } = Array.Empty<string>();
         
-        [Description("Messages containing any of these strings will NOT be printed.")]
+        [Description("Messages containing this string will not be printed. Can be added multiple times")]
         [CommandOption("-n|--not-contains")]
         public string[] NotContains { get; set; } = Array.Empty<string>();
     }
